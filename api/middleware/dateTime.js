5ce31be312d5f2f5d271b759bbfd1cdbfe54476f
Ryan044 <ryan@cgpearson.com>
2018-11-21 12:41:40 -0500

    datetime middleware addition

---
+++ api/middleware/dateTime.js
@@ -0,0 +1,15 @@
+const dateTime = require('date-and-time');
+
+module.exports = (req, res, next) => {
+    try{
+        let now = new Date();
+        //console.log(dateTime.format(now, 'YYYY/MM/DD HH:mm:ss'));
+        req.currentDateTime = dateTime.format(now, 'YYYY/MM/DD HH:mm:ss');
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Date failed'
+        });
+    }
+    
+};
