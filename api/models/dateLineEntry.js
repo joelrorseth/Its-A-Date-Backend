aee55416ee7fa421891bde2c2631328bcf49cdf1
Ryan Pearson <ryan@cgpearson.com>
2018-11-24 10:59:00 -0500

    file add

---
+++ api/models/dateLineEntry.js
@@ -0,0 +1,18 @@
+//dateLineEntry model
+const mongoose = require('mongoose');
+
+//date Schema
+const dateLineEntrySchema = mongoose.Schema({
+
+    _id: mongoose.Schema.Types.ObjectId, 
+    date: { type: mongoose.Schema.Types.ObjectId, ref: 'Date', required: true },
+    lcoation: { type: mongoose.Schema.Types.ObjectId, ref: 'Location', required: true },
+    name: { type: String, required: true },
+    comments: { type: String, required: true },
+    rating: { type: String, required: true },
+    dateCreated: {type: String, required: true },
+    dateUpdated: {type: String, required: true }
+
+})
+
+module.exports = mongoose.model('DateLineEntry', dateLineEntrySchema);
